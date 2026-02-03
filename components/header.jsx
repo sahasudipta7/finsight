a25cce54c69b760e77dfe10fc9918e182b7305ca
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2026-02-03 21:03:40 +0530

    Add sign-in and sign-up components with clerk authorisation

---
+++ components/header.jsx
@@ -0,0 +1,28 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+
+
+const Header = () => {
+  return (
+    <div className="fixed top-0">
+      <nav>
+        <Link href="/">
+          <Image src={"logo.png"} alt="welth logo"
+          height ={60} width={200}
+          className="h-12 w-auto object-contain"
+          />
+        </Link>
+      </nav>
+
+      <SignedOut>
+        <SignInButton />
+        </SignedOut>
+        <SignedIn>
+        <UserButton />
+        </SignedIn>
+    </div>
+  )
+}
+
+export default Header
