de3606bcf148ee2f6deb2715a3a590cdfbf7ee43
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2026-02-04 10:40:50 +0530

    Design logo, remove obsolete files

--- components/header.jsx
+++ components/header.jsx
@@ -1,26 +1,32 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "./ui/button"
 
 
 const Header = () => {
   return (
-    <div className="fixed top-0">
-      <nav>
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
-          <Image src={"logo.png"} alt="welth logo"
-          height ={60} width={200}
+          <Image src={"/logo.png"} alt="welth logo"
+          height ={60} width={100}
           className="h-12 w-auto object-contain"
           />
         </Link>
-      </nav>
+
 
       <SignedOut>
-        <SignInButton />
+        <SignInButton forceRedirectUrl="/dashboard">
+          <Button variant="outline">Login</Button>
+        </SignInButton> 
+
+        
         </SignedOut>
         <SignedIn>
         <UserButton />
         </SignedIn>
+      </nav>
     </div>
   )
 }
